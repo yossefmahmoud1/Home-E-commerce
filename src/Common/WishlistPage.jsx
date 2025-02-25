c81042cbf80159c8cf61441ab639c3855f563617
yossefmahmoud1bashنسختحرير <omaryosef136@gmail.com>
2025-02-25 21:46:48 +0200

    Fixed file name case issue in wishlistSlice

--- src/Common/WishlistPage.jsx
+++ src/Common/WishlistPage.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { removeFromWishlist } from "../redux/wishlistSlice.jsx";
+import { removeFromWishlist } from "../redux/WishlistSlice";
 import { FaHeart } from "react-icons/fa";
 
 const Wishlist = () => {
