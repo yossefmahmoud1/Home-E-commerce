4d2153a56bd487e57c7b2082d028bc8982185b3e
yossefmahmoud1bashنسختحرير <omaryosef136@gmail.com>
2025-02-25 21:33:31 +0200

    Fixed import issue in WishlistPage.jsx

--- src/Common/WishlistPage.jsx
+++ src/Common/WishlistPage.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { removeFromWishlist } from "../redux/wishlistSlice";
+import { removeFromWishlist } from "../redux/wishlistSlice.jsx";
 import { FaHeart } from "react-icons/fa";
 
 const Wishlist = () => {
