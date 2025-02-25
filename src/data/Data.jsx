4834ea1b6e01a8f80e77766ce28823b853e92699
yossefmahmoud1bashنسختحرير <omaryosef136@gmail.com>
2025-02-25 22:47:14 +0200

    Updated image paths for Netlify deployment

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -10,7 +10,6 @@ export const nav = [
 ];
 
 export const aweicon = [
-  
   { icon: <FaFacebookF className="mr-4" /> },
   { icon: <FaInstagram className="mr-4" /> },
   { icon: <FaYoutube className="mr-4" /> },
@@ -18,49 +17,46 @@ export const aweicon = [
 ];
 
 export const sliderdata = [
-  { img: "../../public/img/Hero/Heroimg.jpg" },
-  { img: "../../public/img/Hero/Heroimg2.jpg" },
-  { img: "../../public/img/Hero/Heroimg3.jpg" },
-  { img: "../../public/img/Hero/Heroimg4.jpg" },
-  { img: "../../public/img/Hero/id1-banner2.jpg" },
-
+  { img: "/img/Hero/Heroimg.jpg" },
+  { img: "/img/Hero/Heroimg2.jpg" },
+  { img: "/img/Hero/Heroimg3.jpg" },
+  { img: "/img/Hero/Heroimg4.jpg" },
+  { img: "/img/Hero/id1-banner2.jpg" },
 ];
 
 export const category = [
-  { img: "../img/category/category1.png", category: "Bedroom" },
-  { img: "../img/category/category7.png", category: "Office" },
-  { img: "../img/category/category6.png", category: "Lighting" },
-  { img: "../img/category/category5.png", category: "Bathroom" },
-  { img: "../img/category/category4.png", category: "Kitchen" },
-  { img: "../img/category/category3.png", category: "Interior" },
-  { img: "../img/category/category2.png", category: "Livingroom" },
+  { img: "/img/category/category1.png", category: "Bedroom" },
+  { img: "/img/category/category7.png", category: "Office" },
+  { img: "/img/category/category6.png", category: "Lighting" },
+  { img: "/img/category/category5.png", category: "Bathroom" },
+  { img: "/img/category/category4.png", category: "Kitchen" },
+  { img: "/img/category/category3.png", category: "Interior" },
+  { img: "/img/category/category2.png", category: "Livingroom" },
 ];
 
 export const products = [
-  { id: 1, img: "/public/img/product/Product-1.jpg", name: "SOFA", price: "65.00" },
-  { id: 1, img: "/public/img/product/product-2.jpg", name: "LAMP", price: "65.00" },
-
-  { id: 1, img: "/public/img/product/product1.jpg", name: "BEDROOM", price: "65.00" },
-  { id: 2, img: "/public/img/product/product2.jpg", name: "OFFICE", price: "65.00" },
-  { id: 3, img: "/public/img/product/product3.jpg", name: "LIGHTING", price: "65.00" },
-  { id: 4, img: "/public/img/product/product4.jpg", name: "BATHROOM", price: "65.00" },
-  { id: 5, img: "/public/img/product/product5.jpg", name: "KITCHEN", price: "65.00" },
-  { id: 6, img: "/public/img/product/product6.jpg", name: "INTERIOR", price: "65.00" },
-  { id: 7, img: "/public/img/product/product7.jpg", name: "LIVING ROOM", price: "65.00" },
-  { id: 8, img: "/public/img/product/product8.jpg", name: "DECOR", price: "65.00" },
-  { id: 9, img: "/public/img/product/product9.jpg", name: "LIVING ROOM", price: "65.00" },
-  { id: 10, img: "/public/img/product/product10.jpg", name: "SOFA", price: "65.00" },
-  { id: 11, img: "/public/img/product/product11.jpg", name: "LIVING ROOM", price: "65.00" },
-  { id: 13, img: "/public/img/product/product12.jpg", name: "SOFA", price: "65.00" },
+  { id: 1, img: "/img/product/Product-1.jpg", name: "SOFA", price: "65.00" },
+  { id: 3, img: "/img/product/product1.jpg", name: "BEDROOM", price: "65.00" },
+  { id: 4, img: "/img/product/product2.jpg", name: "OFFICE", price: "65.00" },
+  { id: 5, img: "/img/product/product3.jpg", name: "LIGHTING", price: "65.00" },
+  { id: 6, img: "/img/product/product4.jpg", name: "BATHROOM", price: "65.00" },
+  { id: 7, img: "/img/product/product5.jpg", name: "KITCHEN", price: "65.00" },
+  { id: 8, img: "/img/product/product6.jpg", name: "INTERIOR", price: "65.00" },
+  { id: 9, img: "/img/product/product7.jpg", name: "LIVING ROOM", price: "65.00" },
+  { id: 10, img: "/img/product/product8.jpg", name: "DECOR", price: "65.00" },
+  { id: 11, img: "/img/product/product9.jpg", name: "LIVING ROOM", price: "65.00" },
+  { id: 12, img: "/img/product/product10.jpg", name: "SOFA", price: "65.00" },
+  { id: 13, img: "/img/product/product11.jpg", name: "LIVING ROOM", price: "65.00" },
+  { id: 14, img: "/img/product/product12.jpg", name: "SOFA", price: "65.00" },
 ];
 
 export const brand = [
-  { brand: "../img/brand/br1.png" },
-  { brand: "../img/brand/br2.png" },
-  { brand: "../img/brand/br3.png" },
-  { brand: "../img/brand/br4.png" },
-  { brand: "../img/brand/br5.png" },
-  { brand: "../img/brand/br6.png" },
+  { brand: "/img/brand/br1.png" },
+  { brand: "/img/brand/br2.png" },
+  { brand: "/img/brand/br3.png" },
+  { brand: "/img/brand/br4.png" },
+  { brand: "/img/brand/br5.png" },
+  { brand: "/img/brand/br6.png" },
 ];
 
 export const footer = [
